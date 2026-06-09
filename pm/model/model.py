12fbffd72ca7dc30c024783665070037a41a3c18
Easton Sell <ssell@n0166.talapas.uoregon.edu>
2026-06-08 22:53:28 -0700

    Update project files

--- pm/model/model.py
+++ pm/model/model.py
@@ -6,14 +6,30 @@ class AudioClassifier(nn.Module):
     def __init__(self, num_classes=2):
         super().__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(1, 8, kernel_size=3, padding=1),
+            nn.Conv2d(1, 16, kernel_size=3, padding=1),
+            nn.BatchNorm2d(16),
             nn.ReLU(),
             nn.MaxPool2d(2),
-            nn.Conv2d(8, 16, kernel_size=3, padding=1),
+            
+            nn.Conv2d(16, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
+            nn.MaxPool2d(2),
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout2d(0.2),
+            
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout2d(0.2),
+   
             nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten(),
-            nn.Linear(16, num_classes),
+            nn.Dropout(0.4),
+            nn.Linear(128, num_classes),
         )
 
     def forward(self, x):
